8d55ec907d9308e57e80ee135df3aa22e9af5be9
sxnkush <154624873+sxnkush@users.noreply.github.com>
2024-06-30 23:51:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getTemp = async() => {
     const location = document.getElementById("location").value
     document.getElementById("location").value = null
     const [city, state_code='', country_code=''] = location.split(',').map(item => item.trim());
-    const country = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state_code},${country_code}&limit=5&appid=bb4da9c6051404c62d9d30025b8b91be`)
+    const country = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${state_code},${country_code}&limit=5&appid=bb4da9c6051404c62d9d30025b8b91be`)
     
     const ans = await country.json()
     
@@ -21,9 +21,9 @@ const getTemp = async() => {
     let temperature_class = document.getElementsByClassName("temperature")[0]
 
     if(temp.weather[0].description.match(/sunny/i) || temp.weather[0].description.match(/clear/i) )
-        { 
-            temperature_class.innerHTML = `<img src = "./sunny.svg"><h1>${temp.main.temp}&degC</h2>`
-        }
+    { 
+        temperature_class.innerHTML = `<img src = "./sunny.svg"><h1>${temp.main.temp}&degC</h2>`
+    }
     else
     { 
         temperature_class.innerHTML = `<img src = "./clouds.svg"><h1>${temp.main.temp}&degC</h2>`
@@ -33,11 +33,11 @@ const getTemp = async() => {
     console.log(temp.weather[0].description)
     
     
-    const poll = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`)
+    const poll = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`)
     const air_poll = await poll.json()
     console.log(air_poll.list[0].main.aqi)
 
-    const forecast_5 = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}`)
+    const forecast_5 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}`)
     const ans_forecast = await forecast_5.json()
     console.log(ans_forecast)
 }
